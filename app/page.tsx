b245ebc1062edbdd2348a3563a883eb9584cd448
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-09-05 07:40:06 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Ex1 from './Ex1'
 import Ex2 from './Ex2'
 import Ex3 from './Ex3'
 import Ex6 from './Ex6'
-import Ex7 from './Ex7'
 import Ex9 from './Ex9'
 import Ex10 from './Ex10'
 export default function page() {
@@ -16,7 +15,6 @@ export default function page() {
       <Ex2/>
       <Ex3/>
       <Ex6/>
-      <Ex7/>
       <Ex9/>
       <Ex10/>
       <Footer/>
